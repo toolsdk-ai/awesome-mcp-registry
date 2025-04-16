dd13275f7f7c5ed0fdc6c05606a583313f0ad6f2
Kelly Peilin Chan <kelly@aitable.ai>
2025-04-17 01:06:52 +0800

    fix

--- scripts/check-config.ts
+++ scripts/check-config.ts
@@ -5,9 +5,8 @@
 
 import featured from '../config/featured.mjs';
 import allPackagesList from '../indexes/packages-list.json';
-
-
-for (const f of featured) {
+import verified from '../config/verified.mjs';
+for (const f of [...verified, ...featured]) {
   if (!allPackagesList[f]) {
     throw new Error(`Package ${f} is not in packages-list.json`);
   }
