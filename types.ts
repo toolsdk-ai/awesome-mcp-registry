22fcbf7d1865718524f844e3defae9a1050df17a
Kelly Peilin Chan <kelly@aitable.ai>
2025-04-17 22:38:53 +0800

    chore: add type `MCPServerPackageConfig`

--- types.ts
+++ types.ts
@@ -1,6 +1,7 @@
 
 import { z } from 'zod'
-import type { CategoryConfigSchema, PackageConfigSchema } from './schema';
+import type { CategoryConfigSchema, PackageConfigSchema, MCPServerPackageConfigSchema } from './schema';
 
+export type MCPServerPackageConfig= z.infer<typeof MCPServerPackageConfigSchema>;
 export type PackageConfig = z.infer<typeof PackageConfigSchema>;
 export type CategoryConfig = z.infer<typeof CategoryConfigSchema>;
